8a933140a0da33083ba67ee3940751358a36eec8
salih-sengul <salih.sengul@outlook.com>
2025-10-16 01:34:23 +0300

    Improve logic in utility methods and update .gitignore
    Refactored methods in Main.java to add input validation and correct logic for shouldWakeUp, hasTeen, isCatPlaying, and area functions. Updated .gitignore to exclude /target and *.iml files.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,26 +2,49 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println(area(-5.0));
     }
 
     public static boolean shouldWakeUp(boolean isBarking, int clock) {
-        
+
+        boolean rreturn = false;
+
+        if(!isBarking) return false;
+        if(clock<0 || clock>23) return false;
+
+        if(clock<8) rreturn= true;
+        if(clock>8) rreturn = true;
+        return rreturn;
+
     }
 
     public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
-       
+
+
+       return (firstAge<=19 && firstAge >=13 || secondAge<=19 && secondAge >=13 || thirdAge<=19 && thirdAge >=13);
     }
 
     public static boolean isCatPlaying(boolean isSummer, int temp) {
-     
+     boolean play = false;
+
+     if(isSummer){
+         play = temp<=45 && temp>=25;
+
+     }else{
+         play = temp<=35 && temp>=25;
+     }
+     return play;
     }
 
     public static double area(double width, double height) {
-       
+       if(width<0 || height<0) return -1;
+       return width*height;
     }
 
     public static double area(double radius) {
-       
+       if(radius<0) return -1;
+
+       return radius*radius* Math.PI;
     }
 }
