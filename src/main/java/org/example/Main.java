b6d029bb00e1ce2257d5886d3ed6f0888883fc0c
salih-sengul <salih.sengul@outlook.com>
2025-10-18 04:14:34 +0300

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,7 @@ public class Main {
         System.out.println(area(-5.0));
     }
 
-    
+
     public static boolean shouldWakeUp(boolean isBarking, int clock) {
 
         boolean rreturn = false;
